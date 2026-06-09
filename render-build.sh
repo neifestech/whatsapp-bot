f5722ababc8350249df3d36d5683ff6159b130c1
neifestech <mrwingedman@gmail.com>
2026-06-10 00:01:52 +0500

    Add files via upload

---
+++ render-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o errexit
+
+npm install
+
+PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+mkdir -p $PUPPETEER_CACHE_DIR
+
+npx puppeteer browsers install chrome
+
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
+  echo "...Copying Puppeteer Cache from Build Cache"
+  cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+else
+  echo "...Storing Puppeteer Cache in Build Cache"
+  cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
+fi
